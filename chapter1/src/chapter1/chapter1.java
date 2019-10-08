878c05dc875365f4d97573ceedf162ecbc00c8c8
DavidGirard117 <56056431+DavidGirard117@users.noreply.github.com>
2019-10-07 21:15:42 -0700

    Chapter 1 exercise 2

---
+++ chapter1/src/chapter1/chapter1.java
@@ -0,0 +1,26 @@
+//Header
+//Program Name: Chapter 1 Exercise 2
+//Author: David Girard
+//Class: Fall 2019
+//Date: 10/8/19
+//Description: Compiling and running Java	
+
+package chapter1;
+public class chapter1 {
+
+	public static void main(String[] args) {
+		
+	System.out.println("Hello world"); //First line
+	System.out.println("How are you?\nShe said \"Hello\" to me.\n");
+		//Second line with an escape sequence
+		
+
+	}
+
+}
+	//Footer
+	
+	//Hello world
+	//How are you?
+	//She said "Hello" to me.
+	
